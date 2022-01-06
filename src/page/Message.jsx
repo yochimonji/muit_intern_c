def184b4686822e90d366614d470d384c4956d51
yochimonji <yochimonji@gmail.com>
2022-01-06 17:01:20 +0000

    add メッセージのテキストとユーザーIDを表示

--- src/page/Message.jsx
+++ src/page/Message.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { Container, Stack } from "react-bootstrap";
+
 const Message = () => {
-    return <h2>Message</h2>
+    const [messageList, setMessageList] = useState([]);
+
+    useEffect(()=>{
+        const newMessageList = [
+            { "text": "こんにちは", "userid": "userA" },
+            { "text": "こんにちは", "userid": "userB" },
+            { "text": "よろしくお願いします〜!", "userid": "userA" },
+            { "text": "(^-^)", "userid": "userB" },
+        ]
+        setMessageList(newMessageList)
+    }, [])
+
+    return (
+        <Container>
+            {messageList &&
+                messageList.map((message, index) => 
+                    <Stack gap={5} key={index}>
+                        <div>{message.userid}</div>
+                        <div>{message.text}</div>
+                    </Stack>
+                )
+            }
+        </Container>
+    )
 }
 
 export default Message;
