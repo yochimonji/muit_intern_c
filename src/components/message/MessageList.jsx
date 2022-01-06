524f5beac0e1d468a4b4847059aec96cd115f8ad
yochimonji <yochimonji@gmail.com>
2022-01-06 18:53:54 +0000

    fix 余分なreturn文を削除

--- src/components/message/MessageList.jsx
+++ src/components/message/MessageList.jsx
@@ -1,27 +1,24 @@
 import { Stack } from "react-bootstrap";
 
-const MessageList = (props) => {
-    return (
-        <>
-            {
-                props.messageList.map((message, index) => 
-                    message.userid === props.currentUserId ?
-                    <Stack key={index} className="pb-2">
-                        <div className="justify-content-end d-flex pb-1">{message.userid}</div>
-                        <div className="d-flex justify-content-end">
-                            <div className="bg-dark text-light rounded border p-2">{message.text}</div>
-                        </div>
-                    </Stack> :
-                    <Stack key={index} className="pb-3">
-                        <div className="pb-1">{message.userid}</div>
-                        <div className="d-flex">
-                            <div className="bg-light rounded border p-2">{message.text}</div>
-                        </div>
-                    </Stack>
-                )
-            }
-        </>
-    )
-}
+const MessageList = (props) => 
+    <>
+        {
+            props.messageList.map((message, index) => 
+                message.userid === props.currentUserId ?
+                <Stack key={index} className="pb-2">
+                    <div className="justify-content-end d-flex pb-1">{message.userid}</div>
+                    <div className="d-flex justify-content-end">
+                        <div className="bg-dark text-light rounded border p-2">{message.text}</div>
+                    </div>
+                </Stack> :
+                <Stack key={index} className="pb-3">
+                    <div className="pb-1">{message.userid}</div>
+                    <div className="d-flex">
+                        <div className="bg-light rounded border p-2">{message.text}</div>
+                    </div>
+                </Stack>
+            )
+        }
+    </>
 
 export default MessageList;
