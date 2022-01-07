92a2013f9e57cc80dbc919621106001a68eaf7a4
yochimonji <yochimonji@gmail.com>
2022-01-07 16:03:40 +0000

    add 入力文とメッセージを重複させないようにした

--- src/components/message/MessageSubmit.jsx
+++ src/components/message/MessageSubmit.jsx
@@ -1,13 +1,17 @@
 import { Container, InputGroup, FormControl, Button } from "react-bootstrap";
 
 const MessageSubmit = (props) => 
-    <Container className="fixed-bottom bg-white">
-        <InputGroup className="mb-2">
-            <FormControl className="mr-2" value={props.text} onChange={(e) => props.setText(e.target.value)}/>
-            <Button variant="outline-secondary" onClick={props.submitText}>
-                送信
-            </Button>
-        </InputGroup>
-    </Container>
+    <>
+        <Container className="fixed-bottom bg-white">
+            <InputGroup className="mb-2">
+                <FormControl className="mr-2 " value={props.text} onChange={(e) => props.setText(e.target.value)}/>
+                <Button variant="outline-secondary" onClick={props.submitText}>
+                    送信
+                </Button>
+            </InputGroup>
+        </Container>
+        {/* 入力文とメッセージを重複させないようにするための汚い処理 */}
+        <div className="mb-4 text-white">a</div>
+    </>
     
 export default MessageSubmit;
